c50c884e3331e6370d58d81f4f921be86ce497ab
latsruk <latsruk@gmail.com>
2024-08-25 14:04:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+Number_of_completed_homework=12
+Number_of_hours_spent=1.5
+Course_Title='Python'
+print('Курс:',Course_Title,',всего задач:',Number_of_completed_homework,', затрачено часов:',Number_of_hours_spent,
+', среднее время выполнения',(Number_of_hours_spent/Number_of_completed_homework,'часа'))
