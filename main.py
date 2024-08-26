1a1356121005961bd36e372b1a3537a719f749d4
latsruk <latsruk@gmail.com>
2024-08-26 19:43:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-Number_of_completed_homework=12
-Number_of_hours_spent=1.5
-Course_Title='Python'
+Number_of_completed_homework=12 # Количество выполненных ДЗ
+Number_of_hours_spent=1.5 # Количество затраченных часов
+Course_Title='Python' # Название курса
+time_task = (Number_of_completed_homework and Number_of_hours_spent ) # Время на одно задание
 print('Курс:',Course_Title,',всего задач:',Number_of_completed_homework,', затрачено часов:',Number_of_hours_spent,
 ', среднее время выполнения',(Number_of_hours_spent/Number_of_completed_homework,'часа'))
